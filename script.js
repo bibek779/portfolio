e3014fa58231e66ce5a815a5e012249eb7dd126c
bibek779 <118890208+bibek779@users.noreply.github.com>
2025-07-08 11:38:08 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+  
